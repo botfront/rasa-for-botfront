1dc2a1d77d0be0e1a5ebd86f1b50d6b39cff8f7d
Theodore Tomalty <theo@mrbot.ai>
2018-09-25 13:44:34 -0400

    changed config, now one entry per entity

--- rasa_nlu/postprocessors/fuzzy_gazette.py
+++ rasa_nlu/postprocessors/fuzzy_gazette.py
@@ -23,15 +23,23 @@ def _find_matches(query, gazette, mode="ratio", limit=5):
     return output
 
 
+def _find_entity_config(entity, config):
+    for rep in config.get("entities", []):
+        if entity["entity"] == rep["name"]:
+            return rep
+
+    return None
+
+
 class FuzzyGazette(Component):
     name = "fuzzy_gazette"
 
-    provides = ["entities", "additional_info"]
+    provides = ["entities"]
 
     defaults = {
-        "minimum_score": 80,
+        "extractors": ["ner_crf"],
         "max_num_suggestions": 5,
-        "mode": "ratio"
+        "entities": [],
     }
 
     def __init__(self, component_config=None, gazette=None):
@@ -43,28 +51,31 @@ class FuzzyGazette(Component):
     def process(self, message, **kwargs):
         # type: (Message, **Any) -> None
 
+        self._load_config()
         entities = message.get("entities", [])
-        mode = self.component_config.get("mode")
         limit = self.component_config.get("max_num_suggestions")
 
         for entity in entities:
-            if not isinstance(entity["value"], str):
+            right_extractor = "extractor" in entity and entity["extractor"] in self.component_config["extractors"]
+            config = _find_entity_config(entity, self.component_config)
+
+            if not isinstance(entity["value"], str) or not right_extractor or config is None:
                 continue
 
-            matches = _find_matches(entity["value"], self.gazette, mode=mode, limit=limit)
+            matches = _find_matches(entity["value"], self.gazette, mode=config["mode"], limit=limit)
             top_matches = []
             for key, val in matches.items():
                 for item in val:
                     top_matches.append((key, *item))
             top_matches.sort(key=lambda x: x[2])
 
-            key, primary, score = top_matches.pop() if len(top_matches) else (None, None, None)
+            key, primary, score = top_matches[-1] if len(top_matches) else (None, None, None)
 
-            if primary is not None and score > self.component_config.get("minimum_score"):
+            if primary is not None and score > config["min_score"]:
                 entity["entity"] = key
                 entity["value"] = primary
 
-                entity["additional_info"] = [{"entity": entity, "value": value, "score": num} for entity, value, num in top_matches]
+                entity["gazette_best_matches"] = [{"entity": entity, "value": value, "score": num} for entity, value, num in top_matches]
 
         message.set("entities", entities)
 
@@ -95,11 +106,10 @@ class FuzzyGazette(Component):
         from rasa_nlu.utils import read_json_file
 
         meta = model_metadata.for_component(cls.name)
-        file_name = meta.get("synonyms_file", FUZZY_GAZETTE_FILE)
+        file_name = meta.get("gazette_file", FUZZY_GAZETTE_FILE)
         path = os.path.join(model_dir, file_name)
 
         gazette = read_json_file(path)
-
         return FuzzyGazette(meta, gazette)
 
     def _load_gazette_list(self, gazette):
@@ -112,3 +122,24 @@ class FuzzyGazette(Component):
                 self.gazette[name] += table
             else:
                 self.gazette[name] = table
+
+    def _load_config(self):
+        entities = []
+        for rep in self.component_config.get("entities", []):
+            assert "name" in rep, "Must provide the entity name for the gazette entity configuration: {}".format(rep)
+            assert rep["name"] in self.gazette, "Could not find entity name {0} in gazette {1}".format(rep["name"], self.gazette)
+
+            supported_properties = ["entities", "mode", "min_score"]
+            defaults = [[], "ratio", 80]
+
+            new_element = {"name": rep["name"]}
+            for prop, default in zip(supported_properties, defaults):
+                if prop not in rep:
+                    new_element[prop] = default
+                else:
+                    assert isinstance(rep[prop], type(default)), "The type of {0} in {1} does not match the default".format(prop, rep["name"])
+                    new_element[prop] = rep[prop]
+
+            entities.append(new_element)
+
+        self.component_config["entities"] = entities
