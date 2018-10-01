6c670c69d3d9a39c1818dc6fdc1976840d169c19
Theodore Tomalty <theo@mrbot.ai>
2018-10-01 15:19:58 -0400

    test file

---
+++ tests/base/test_preprocessors.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from rasa_nlu.preprocessors.spell_check import BingSpellCheck
+from rasa_nlu.training_data.message import Message
+
+from pytest import raises
+
