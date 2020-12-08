09f8548f4f0d6f50159d01602ddc59e045677154
Kenton <kenton.rilea@mrbot.ai>
2020-12-08 11:24:57 -0500

    refactor: remove un-used imports

--- tests/addons/core/test_botfront_testing_channel.py
+++ tests/addons/core/test_botfront_testing_channel.py
@@ -1,29 +1,8 @@
-import json
-import logging
-from typing import Dict
-from unittest.mock import patch, MagicMock, Mock
 
 import pytest
-from _pytest.monkeypatch import MonkeyPatch
-from aiohttp import ClientTimeout
-from aioresponses import aioresponses
-from sanic import Sanic
 
-import rasa.core.run
-from rasa.core import utils
-from rasa.core.channels import RasaChatInput, console
-from rasa.core.channels.channel import UserMessage
-from rasa.core.channels.rasa_chat import (
-    JWT_USERNAME_KEY,
-    CONVERSATION_ID_KEY,
-    INTERACTIVE_LEARNING_PERMISSION,
-)
-from rasa.core.channels.telegram import TelegramOutput
-from rasa.utils.endpoints import EndpointConfig
-from tests.core import utilities
 
 # this is needed so that the tests included as code examples look better
-from tests.utilities import json_of_latest_request, latest_request
 from tests.addons.core.conftest import botfront_test_channel_test_data
 
 def test_botframework_channel():
