0a67ca5f1e39c57a663368afddb1a638065016b9
Philippe Cote-Boucher <philippe@mrbot.ai>
2020-12-08 14:40:49 -0500

    refactor: file renames

--- rasa/core/processor.py
+++ rasa/core/processor.py
@@ -239,7 +239,7 @@ class MessageProcessor:
             if should_save_tracker:
                 # save tracker state to continue conversation from this state
                 self._save_tracker(tracker)
-        if message.output_channel.name() == 'botfront_test_output':
+        if message.output_channel.name() == 'bot_regression_test_output':
             # BOTFRONT TEST CHANNEL: send user messages to the output channel
             # if the output channel is the botfront output channel
             message.output_channel.send_parsed_message(tracker.latest_message)
--- rasa_addons/core/channels/bot_regession_test.py
+++ rasa_addons/core/channels/bot_regession_test.py
@@ -1,29 +1,26 @@
-import asyncio
 import rasa
-import os
 import logging
 import inspect
-from rasa.core.channels.channel import UserMessage, CollectingOutputChannel, InputChannel
+from rasa.core.channels.channel import (
+    UserMessage,
+    InputChannel,
+)
 from rasa.core.channels.rest import RestInput
 from sanic.request import Request
-from sanic import Sanic, Blueprint, response
-from asyncio import Queue, CancelledError
-from typing import Text, List, Dict, Any, Optional, Callable, Iterable, Awaitable
-from rasa.core import utils
+from sanic import Blueprint, response
+from asyncio import CancelledError
+from typing import Text, List, Dict, Any, Optional, Callable, Awaitable
 from sanic.response import HTTPResponse
-from rasa_addons.core.channels.graphql import get_config_via_graphql
 from rasa_addons.core.channels.rest import BotfrontRestOutput
 from datetime import datetime
-from rasa.shared.core.events import UserUttered
-from sgqlc.endpoint.http import HTTPEndpoint
-import urllib.error
 
 logger = logging.getLogger(__name__)
 
-class TestCaseOutput(BotfrontRestOutput):
-    def name(csl) -> Text:
-        return "botfront_test_output"
-    
+
+class BotRegressionTestOutput(BotfrontRestOutput):
+    def name(self) -> Text:
+        return "bot_regression_test_output"
+
     @staticmethod
     def extract_entities(entities: List[Dict[Text, Any]]) -> List[Dict[Text, Any]]:
         formatted_entities = []
@@ -37,10 +34,7 @@ class TestCaseOutput(BotfrontRestOutput):
             formatted_entities.append(formatted_entity)
         return formatted_entities
 
-    def send_parsed_message (
-        self,
-        message: UserMessage,
-    ) -> None:
+    def send_parsed_message(self, message: UserMessage,) -> None:
         formatted_message = {
             "user": message.text,
             "intent": message.intent.get("name"),
@@ -49,7 +43,7 @@ class TestCaseOutput(BotfrontRestOutput):
         self.messages.append(formatted_message)
 
 
-class TestCaseInput(RestInput):
+class BotRegressionTestInput(RestInput):
     def __init__(self, config: Optional[Dict[Text, Any]] = None):
         self.url = config.get("url")
 
@@ -59,20 +53,26 @@ class TestCaseInput(RestInput):
         return cls(credentials)
 
     def name(cls) -> Text:
-       return "test_case"
+        return "test_case"
 
     def _extract_test_cases(self, req: Request) -> Text:
         return req.json.get("test_cases")
+
     def _extract_project_id(self, req: Request) -> Text:
         return req.json.get("project_id")
 
-    async def simulate_messages(self, steps: List[Dict[Text, Any]], language: Text, on_new_message: Callable[[UserMessage], Awaitable[None]]) -> List[Dict[Text, Any]]:
-        sender_id = 'botfront_test_case_{:%Y-%m-%d_%H:%M:%S}'.format(datetime.now())
-        collector = TestCaseOutput()
+    async def simulate_messages(
+        self,
+        steps: List[Dict[Text, Any]],
+        language: Text,
+        on_new_message: Callable[[UserMessage], Awaitable[None]],
+    ) -> List[Dict[Text, Any]]:
+        sender_id = "bot_regression_test_{:%Y-%m-%d_%H:%M:%S}".format(datetime.now())
+        collector = BotRegressionTestOutput()
         for step in steps:
             if "user" in step:
                 text = step.get("user")
-                metadata = { "lang": language }
+                metadata = {"lang": language}
                 try:
                     await on_new_message(
                         UserMessage(
@@ -97,60 +97,79 @@ class TestCaseInput(RestInput):
 
     @staticmethod
     def compare_one_entity(actual, expected) -> bool:
-        return actual.get("entity") == expected.get("entity") and actual.get("value") == expected.get("value") and actual.get("start") == expected.get("start") and actual.get("end") == expected.get("end")
+        return (
+            actual.get("entity") == expected.get("entity")
+            and actual.get("value") == expected.get("value")
+            and actual.get("start") == expected.get("start")
+            and actual.get("end") == expected.get("end")
+        )
 
     def compare_entities(self, actual_entities, expected_entites) -> bool:
         expected_remaining = expected_entites.copy()
         for actual in actual_entities:
-            index_of_match = next((i for i, expected in enumerate(expected_remaining) if self.compare_one_entity(actual, expected)), -1)
+            index_of_match = next(
+                (
+                    i
+                    for i, expected in enumerate(expected_remaining)
+                    if self.compare_one_entity(actual, expected)
+                ),
+                -1,
+            )
             if index_of_match in range(0, len(expected_remaining)):
                 expected_remaining.pop(index_of_match)
-        return len(expected_remaining) == 0 and len(actual_entities) == len(expected_entites)
+        return len(expected_remaining) == 0 and len(actual_entities) == len(
+            expected_entites
+        )
 
     def compare_steps(self, actual_step, expected_step) -> bool:
         if "user" in actual_step and "user" in expected_step:
-            return (actual_step.get("text") == expected_step.get("text")
-            and actual_step.get("intent") == expected_step.get("intent")
-            and self.compare_entities(actual_step.get("entities"), expected_step.get("entities")))
+            return (
+                actual_step.get("text") == expected_step.get("text")
+                and actual_step.get("intent") == expected_step.get("intent")
+                and self.compare_entities(
+                    actual_step.get("entities"), expected_step.get("entities")
+                )
+            )
         elif "action" in actual_step and "action" in expected_step:
             return actual_step.get("action") == expected_step.get("action")
-        else: return False
-    
+        else:
+            return False
+
     @staticmethod
     def format_as_step(step: Dict[Text, Any]) -> Dict[Text, Any]:
         if "metadata" in step and "template_name" in step.get("metadata"):
-            return { "action": step.get("metadata").get("template_name") }
-        else: return step
+            return {"action": step.get("metadata").get("template_name")}
+        else:
+            return step
 
     def get_index_of_step(
-        self,
-        step: Dict[Text, Any],
-        step_list: List[Dict[Text, Any]],
+        self, step: Dict[Text, Any], step_list: List[Dict[Text, Any]],
     ) -> int:
-        return next((
-            i for i, current_step
-            in enumerate(step_list)
-            if self.compare_steps(step, current_step)
-        ), None)
+        return next(
+            (
+                i
+                for i, current_step in enumerate(step_list)
+                if self.compare_steps(step, current_step)
+            ),
+            None,
+        )
 
     @staticmethod
     def accumulate_actual_step(
-        step: Dict[Text, Any],
-        results_acc: Dict[Text, Any],
+        step: Dict[Text, Any], results_acc: Dict[Text, Any],
     ) -> Dict[Text, Any]:
         step_to_append = step.copy()
         step_to_append["theme"] = "actual"
         results_acc.get("actual").append(step_to_append)
-    
+
     @staticmethod
     def accumulate_matching_step(
-        step: Dict[Text, Any],
-        results_acc: Dict[Text, Any],
+        step: Dict[Text, Any], results_acc: Dict[Text, Any],
     ) -> Dict[Text, Any]:
         results_acc.get("steps").extend(results_acc.get("expected"))
         results_acc["expected"] = []
         results_acc.get("steps").append(step)
-    
+
     @staticmethod
     def accumulate_expected_steps(
         expected_steps: List[Dict[Text, Any]],
@@ -179,39 +198,46 @@ class TestCaseInput(RestInput):
             elif match_index in range(0, len(expected_steps_remaining)):
                 if match_index > 0:
                     self.accumulate_expected_steps(
-                        expected_steps_remaining,
-                        match_index,
-                        results_acc
+                        expected_steps_remaining, match_index, results_acc
                     )
                 matched_step = expected_steps_remaining.pop(0)
                 self.accumulate_matching_step(matched_step, results_acc)
-            print('step complete')
+            print("step complete")
         # accumulate expected steps will add any leftover expected/actual steps to results_acc.steps
         self.accumulate_expected_steps(
-            expected_steps_remaining,
-            len(expected_steps_remaining),
-            results_acc
+            expected_steps_remaining, len(expected_steps_remaining), results_acc
         )
         return results_acc.get("steps")
-    
+
     @staticmethod
     def check_success(steps: List[Dict[Text, Any]]) -> bool:
         return next((False for step in steps if "theme" in step), True)
 
-    async def run_tests (self, test_cases: List[Dict[Text, Any]], project_id: Text, on_new_message: Callable[[UserMessage], Awaitable[None]]) -> List[Dict[Text, Any]]:
+    async def run_tests(
+        self,
+        test_cases: List[Dict[Text, Any]],
+        project_id: Text,
+        on_new_message: Callable[[UserMessage], Awaitable[None]],
+    ) -> List[Dict[Text, Any]]:
         all_results = []
         for test_case in test_cases:
             expected_steps = test_case.get("steps")
-            collector = await self.simulate_messages(expected_steps, test_case.get("language"), on_new_message)
-            test_results = self.compare_step_lists(collector.messages, test_case.get("steps"))
-            all_results.append({
-                "_id": test_case.get("_id"),
-                "testResults": test_results,
-                "success": self.check_success(test_results),
-                "projectId": project_id,
-            })
+            collector = await self.simulate_messages(
+                expected_steps, test_case.get("language"), on_new_message
+            )
+            test_results = self.compare_step_lists(
+                collector.messages, test_case.get("steps")
+            )
+            all_results.append(
+                {
+                    "_id": test_case.get("_id"),
+                    "testResults": test_results,
+                    "success": self.check_success(test_results),
+                    "projectId": project_id,
+                }
+            )
         return all_results
-    
+
     def blueprint(
         self, on_new_message: Callable[[UserMessage], Awaitable[None]]
     ) -> Blueprint:
@@ -224,7 +250,7 @@ class TestCaseInput(RestInput):
         @custom_webhook.route("/", methods=["GET"])
         async def health(request: Request) -> HTTPResponse:
             return response.json({"status": "ok"})
-        
+
         @custom_webhook.route("/run", methods=["POST"])
         async def receive(request: Request) -> HTTPResponse:
             should_use_stream = rasa.utils.endpoints.bool_arg(
@@ -232,7 +258,7 @@ class TestCaseInput(RestInput):
             )
             test_cases = self._extract_test_cases(request)
             project_id = self._extract_project_id(request)
-            response.json({ "success": True })
+            response.json({"success": True})
             results = await self.run_tests(test_cases, project_id, on_new_message)
             return response.json(results)
 
