999cd7c3c4b0f2d2f39e1b30d68b5d3a6ad42a6f
Philippe Cote-Boucher <philippe@mrbot.ai>
2021-01-27 10:06:34 -0500

    chore: move addons to poetry

--- rasa_addons/setup.py
+++ rasa_addons/setup.py
@@ -1,19 +1,10 @@
 from setuptools import setup, find_packages
-import os
-import io
 
-with io.open(
-    os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md"),
-    encoding="utf-8",
-) as f:
-    long_description = f.read()
 setup(
     name="rasa_addons",
     version="2.2.5.2",
     author="Botfront",
     description="Rasa Addons - Components for Rasa and Botfront",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     install_requires=[
         "requests",
         "requests_futures",
@@ -21,7 +12,7 @@ setup(
         "fbmessenger",
         "sgqlc",
     ],
-    packages=find_packages(include=["rasa_addons*"]),
+    packages=find_packages(),
     licence="Apache 2.0",
     url="https://botfront.io",
     author_email="hi@botfront.io",
