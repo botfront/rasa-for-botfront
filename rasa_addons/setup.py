d3b8ea6c39e925552be61ef49ca2400925bf8286
Philippe Cote-Boucher <philippe@mrbot.ai>
2021-01-29 14:38:28 -0500

    chore: release

--- rasa_addons/setup.py
+++ rasa_addons/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rasa_addons",
-    version="2.2.5.2",
+    version="2.2.5.3",
     author="Botfront",
     description="Rasa Addons - Components for Rasa and Botfront",
     install_requires=[
