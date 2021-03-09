86f33afbb25a935d9de4aa941c2be707a6a7119d
Philippe Cote-Boucher <pcoteboucher@0.edu.kg>
2021-03-09 10:52:15 -0500

    chore: version

--- rasa_addons/setup.py
+++ rasa_addons/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rasa_addons",
-    version="2.3.3.1",
+    version="2.3.3.2",
     author="Botfront",
     description="Rasa Addons - Components for Rasa and Botfront",
     install_requires=[
